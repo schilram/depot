76533f12e5ec3d601109f65e21a60d64fa665e61
Ramon Schilling <schilram@students.zhaw.ch>
2012-03-27 13:41:55 +0200

    Validation

--- db/migrate/20120312074527_create_products.rb
+++ db/migrate/20120312074527_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :title
+      t.string :title , null: false
       t.text :description
       t.string :image_url
-      t.decimal :price, prcision: 8, scale: 2
+      t.decimal :price, precision: 8, scale: 2
       t.timestamps
     end
   end
