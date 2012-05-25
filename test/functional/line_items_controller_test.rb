c8d1a84f8cd2131b0b0f18f94a23986fed9a97c2
Ramon Schilling <schilram@students.zhaw.ch>
2012-05-25 08:29:06 +0200

    Kap 9

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -18,32 +18,30 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      #post :create, line_item: @line_item.attributes
-			post :create, product_id: products(:ruby).id
+      post :create, product_id: products(:ruby).id
     end
 
-    #assert_redirected_to line_item_path(assigns(:line_item))
-		assert_redirected_to cart_path(assings(:line_item).cart)
+    assert_redirected_to cart_path(assigns(:line_item).cart)
   end
 
   test "should show line_item" do
-    get :show, id: @line_item
+    get :show, id: @line_item.to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @line_item
+    get :edit, id: @line_item.to_param
     assert_response :success
   end
 
   test "should update line_item" do
-    put :update, id: @line_item, line_item: @line_item.attributes
+    put :update, id: @line_item.to_param, line_item: @line_item.attributes
     assert_redirected_to line_item_path(assigns(:line_item))
   end
 
   test "should destroy line_item" do
     assert_difference('LineItem.count', -1) do
-      delete :destroy, id: @line_item
+      delete :destroy, id: @line_item.to_param
     end
 
     assert_redirected_to line_items_path
